7d1b626b6f325c6904a4a9eb8338cc021f8efc55
Ricard Garcia <imricardgarcia@gmail.com>
2021-05-25 16:17:53 +0200

    Added all episodes (2.1.1.)

---
+++ src/api/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+// Used axios.create() to establish a base API to make requests
+function makeAPI() {
+  return axios.create({ baseURL: "https://rickandmortyapi.com/api" });
+}
+
+export function getEpisodes(page = 1, api = makeAPI()) {
+  return api.get(`/episode?page=${page}`);
+}
+
+export function getCharacter(character = 1, api = makeAPI()) {
+  return api.get(`/character/${character}`);
+}
+
+// Codi a Episode
+// const promises = data.characters.map(character) => axios.get(character))
+
+// const charactersResponse = await Promise.all(promises)
+// const characters  = charactersResponse(map(character) => characters.data )
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { getEpisodes, getCharacter } from "../../api";
+import EpisodeCard from "../../components/EpisodeCard";
 
 import Layout from "../../components/Layout";
 // import EpisodeCard from "../../components/EpisodeCard";
@@ -7,44 +9,75 @@ class Home extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
-    // page: 1,
-    // paginationInfo: null,
-    // episodes: [],
-    // hasLoaded: false,
-    // hasError: false,
-    // errorMessage: null,
+    this.state = {
+      page: 1,
+      paginationInfo: null,
+      episodes: [],
+      hasLoaded: false,
+      hasError: false,
+      errorMessage: null,
+    };
+
+    this.loadEpisodes = this.loadEpisodes.bind(this);
   }
 
   async componentDidMount() {
-    // this.loadEpisodes();
+    console.clear();
+    const { page } = this.state;
+    this.loadEpisodes(page);
   }
 
-  async loadEpisodes() {
+  async loadEpisodes(page) {
     console.log(this);
+
+    try {
+      const { data } = await getEpisodes(page);
+      console.log(data);
+
+      this.setState({
+        paginationInfo: data.info,
+        episodes: data.results,
+        hasLoaded: true,
+      });
+    } catch (error) {
+      this.setState({
+        hasLoaded: true,
+        hasError: true,
+        errorMessage: error.message,
+      });
+    }
   }
 
   render() {
+    const {
+      page,
+      paginationInfo,
+      episodes,
+      hasLoaded,
+      hasError,
+      errorMessage,
+    } = this.state;
     return (
       <Layout>
         <section className="row">
-          {/* {hasLoaded && !hasError && (
+          {hasLoaded && !hasError && (
             <div className="col col-12">
               <h1>Episodes loaded!</h1>
             </div>
-          )} */}
+          )}
           <div className="col col-12">
             <hr />
           </div>
-          {/* {episodes.map((episode) => (
-              <EpisodeCard
-                key={episode.id}
-                id={episode.id}
-                name={episode.name}
-                airDate={episode.air_date}
-                episode={episode.episode}
-              />
-            ))} */}
+          {/* <div className="col col-12">{JSON.stringify(episodes, null, 2)}</div> */}
+          {episodes.map((episode) => (
+            <EpisodeCard
+              key={episode.id}
+              id={episode.id}
+              name={episode.name}
+              airDate={episode.air_date}
+              episode={episode.episode}
+            />
+          ))}
           <div className="col col-12">
             <hr />
           </div>
