343552f8a25079f8b4e4190667b32ff8b32c976d
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-05-26 10:34:47 +0200

    Finished character page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import {
+  CHARACTER,
+  EPISODE,
+  EPISODES,
+  HOME,
+  LOCATION,
+} from "./constants/routes";
 
 import Home from "./pages/Home";
+import Episode from "./pages/Episode";
+import Character from "./pages/Character";
 
 function App() {
-  return <Home />;
+  return (
+    <Switch>
+      <Route
+        path={`${EPISODE}/:episodeId`}
+        render={(routeProps) => <Episode {...routeProps} />}
+      />
+      <Route
+        path={`${CHARACTER}/:characterId`}
+        render={(routeProps) => <Character {...routeProps} />}
+      />
+      <Route path={HOME} render={(routeProps) => <Home {...routeProps} />} />
+    </Switch>
+  );
 }
 
 export default App;
