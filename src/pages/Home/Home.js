f200ff36bcac7ca5721976fb716c880edd4e60cd
Alfonso <alfonso.gdlsm@gmail.com>
2021-05-27 16:08:59 +0200

    delivery

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,42 +1,101 @@
 import React, { Component } from "react";
 
+import { getEpisodes } from "../../api";
+
 import Layout from "../../components/Layout";
-// import EpisodeCard from "../../components/EpisodeCard";
+import EpisodeCard from "../../components/EpisodeCard";
 
 class Home extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
-    // page: 1,
-    // paginationInfo: null,
-    // episodes: [],
-    // hasLoaded: false,
-    // hasError: false,
-    // errorMessage: null,
+    this.state = {
+      page: 1,
+      paginationInfo: null,
+      episodes: [],
+      hasLoaded: false,
+      hasError: false,
+      errorMessage: null,
+    };
+
+    this.loadEpisodes = this.loadEpisodes.bind(this);
+    this.loadNextPage = this.loadNextPage.bind(this);
+    this.loadPrevPage = this.loadPrevPage.bind(this);
   }
 
-  async componentDidMount() {
-    // this.loadEpisodes();
+  componentDidMount() {
+    const { page } = this.state;
+    this.loadEpisodes(page);
   }
 
-  async loadEpisodes() {
-    console.log(this);
+  componentDidUpdate(_prevProps, prevState) {
+    const { page: prevPage } = prevState;
+    const { page } = this.state;
+    if (prevPage !== page) {
+      this.loadEpisodes(page);
+    }
+  }
+
+  async loadEpisodes(page) {
+    try {
+      const { data } = await getEpisodes(page);
+      this.setState({
+        paginationInfo: data.info,
+        episodes: data.results,
+        // episodes: [...prevState.episodes, ...data.results],
+        hasLoaded: true,
+      });
+    } catch (e) {
+      this.setState({
+        hasLoaded: true,
+        hasError: true,
+        errorMessage: e.message,
+      });
+    }
+  }
+
+  loadPrevPage() {
+    this.setState((prevState) => ({
+      page: prevState.page - 1,
+    }));
+  }
+
+  loadNextPage() {
+    this.setState((prevState) => ({
+      page: prevState.page + 1,
+    }));
   }
 
   render() {
+    const {
+      paginationInfo,
+      episodes,
+      hasLoaded,
+      hasError,
+      errorMessage,
+    } = this.state;
+
     return (
       <Layout>
         <section className="row">
-          {/* {hasLoaded && !hasError && (
+          {hasLoaded && !hasError && (
             <div className="col col-12">
               <h1>Episodes loaded!</h1>
             </div>
-          )} */}
-          <div className="col col-12">
-            <hr />
-          </div>
-          {/* {episodes.map((episode) => (
+          )}
+          {hasError && (
+            <div className="col col-12">
+              <h1>Something went wrong...</h1>
+              <p>{errorMessage}</p>
+            </div>
+          )}
+          {!hasLoaded && (
+            <div className="col col-12">
+              <h1>Loading episodes...</h1>
+            </div>
+          )}
+          {episodes.length > 0 &&
+            episodes.map((episode) => (
               <EpisodeCard
                 key={episode.id}
                 id={episode.id}
@@ -44,9 +103,27 @@ class Home extends Component {
                 airDate={episode.air_date}
                 episode={episode.episode}
               />
-            ))} */}
+            ))}
           <div className="col col-12">
             <hr />
+          </div>
+          <div className="col col-12 btn-group btn-group-justified">
+            <button
+              className="btn btn-primary"
+              type="button"
+              disabled={paginationInfo && !paginationInfo.prev}
+              onClick={this.loadPrevPage}
+            >
+              Previous page
+            </button>
+            <button
+              className="btn btn-primary"
+              type="button"
+              disabled={paginationInfo && !paginationInfo.next}
+              onClick={this.loadNextPage}
+            >
+              Next page
+            </button>
           </div>
         </section>
       </Layout>
