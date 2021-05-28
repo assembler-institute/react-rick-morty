bd23fa7c472268ad36b3f816dce5d2787f7b7d15
김은영 <solaz0824@gmail.com>
2021-05-28 08:55:48 +0200

    chage color and add location to  CharacterCard

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -5,23 +5,22 @@ import "./CharacterCard.scss";
 
 import * as routes from "../../constants/routes";
 
-function CharacterCard({ id, name, image, species, status, origin }) {
+function CharacterCard({ id, name, image, species, status, origin, location }) {
   return (
     <div className="col col-12 col-sm-6 col-xl-3 CharacterCard">
       <img className="CharacterCard__img" src={image} alt="" />
       <Link to={`${routes.CHARACTER}/${id}`}>
         <h3 className="CharacterCard__name h4">{name}</h3>
       </Link>
-      <div className="CharacterCard__meta">
+      <div className="d-flex">
         <Link
           className="CharacterCard__meta-item"
           to={`${routes.LOCATION}/${id}`}
         >
           {origin.name}
         </Link>
-        <p className="CharacterCard__meta-item">|</p>
-        <p className="CharacterCard__meta-item">{status}</p>
-        <p className="CharacterCard__meta-item">|</p>
+        <p className="CharacterCard__meta-item text-info">{location.name}</p>
+        <p className="CharacterCard__meta-item text-warning">{status}</p>
         <p className="CharacterCard__meta-item text-muted">{species}</p>
       </div>
     </div>
