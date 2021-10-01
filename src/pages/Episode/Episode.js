14a22a26394428db706a6ca9d7b92e171420714b
Pere Serra Bosch <pere.serra.bosch@gmail.com>
2021-10-01 12:43:40 +0200

    episodes

--- src/pages/Episode/Episode.js
+++ src/pages/Episode/Episode.js
@@ -7,12 +7,49 @@ class Episode extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
-    // episode: null,
-    // characters: [],
-    // hasLoaded: false,
-    // hasError: false,
-    // errorMessage: null,
+    this.state = {
+      episode: props.episode,
+      characters: [],
+      hasLoaded: false,
+      hasError: false,
+      errorMessage: null,
+    };
+  }
+
+  loadEpisode = async () => {
+    try {
+      const { episode } = this.state;
+
+      const url = `https://rickandmortyapi.com/api/episode/${episode}`;
+      const result = await fetch(url)
+
+      console.log(result)
+
+      // const { results, info } = await result.json();
+
+      // this.setState((prevState) => ({
+      //   episode: [...prevState.episode, ...results],
+      //   hasLoaded: true,
+      //   characters: []
+      // }))
+
+    } catch (error) {
+      this.setState(() => ({
+        hasError: true,
+        errorMessage: error
+      }))
+    }
+
+    // const pokemons = await fetch(url);
+    // const jsonPokemons = await pokemons.json();
+
+    // let arrayPokemons = await Promise.all(
+    //   jsonPokemons.results.map(async (pokemon) => {
+    //     let pokemonResponse = await fetch(pokemon.url);
+    //     return pokemonResponse.json()
+    //   })
+    // );
+
   }
 
   render() {
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,52 +1,107 @@
 import React, { Component } from "react";
 
 import Layout from "../../components/Layout";
-// import EpisodeCard from "../../components/EpisodeCard";
+import EpisodeCard from "../../components/EpisodeCard";
 
 class Home extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
-    // page: 1,
-    // paginationInfo: null,
-    // episodes: [],
-    // hasLoaded: false,
-    // hasError: false,
-    // errorMessage: null,
+    this.state = {
+      page: 1,
+      paginationInfo: null,
+      episodes: [],
+      hasLoaded: false,
+      hasError: false,
+      errorMessage: null,
+    };
   }
 
   async componentDidMount() {
-    // this.loadEpisodes();
+    this.loadEpisodes();
   }
 
-  async loadEpisodes() {
-    console.log(this);
+  loadNextPage = () => {
+    this.setState((prevState) => ({
+      ...prevState,
+      page: prevState.page + 1,
+    }))
+
+    this.loadEpisodes()
+  }
+
+
+  loadEpisodes = async () => {
+    try {
+      const { page } = this.state;
+
+      const url = `https://rickandmortyapi.com/api/episode?page=${page}`;
+      const result = await fetch(url)
+      const { results, info } = await result.json();
+
+      this.setState((prevState) => ({
+        episodes: [...prevState.episodes, ...results],
+        hasLoaded: true,
+        paginationInfo: info
+      }))
+
+    } catch (error) {
+      this.setState(() => ({
+        hasError: true,
+        errorMessage: error
+      }))
+    }
+
+    // const pokemons = await fetch(url);
+    // const jsonPokemons = await pokemons.json();
+
+    // let arrayPokemons = await Promise.all(
+    //   jsonPokemons.results.map(async (pokemon) => {
+    //     let pokemonResponse = await fetch(pokemon.url);
+    //     return pokemonResponse.json()
+    //   })
+    // );
+
   }
 
   render() {
+    const { episodes, errorMessage, hasLoaded, hasError, page, paginationInfo } = this.state;
+
     return (
       <Layout>
+        {errorMessage}
         <section className="row">
-          {/* {hasLoaded && !hasError && (
+          {hasLoaded && !hasError && (
             <div className="col col-12">
               <h1>Episodes loaded!</h1>
             </div>
-          )} */}
+          )}
           <div className="col col-12">
             <hr />
           </div>
-          {/* {episodes.map((episode) => (
-              <EpisodeCard
-                key={episode.id}
-                id={episode.id}
-                name={episode.name}
-                airDate={episode.air_date}
-                episode={episode.episode}
-              />
-            ))} */}
-          <div className="col col-12">
+          {episodes.map((episode) => (
+            <EpisodeCard
+              key={episode.id}
+              id={episode.id}
+              name={episode.name}
+              airDate={episode.air_date}
+              episode={episode.episode}
+            />
+          ))}
+          <div className="col col-12 text-center">
             <hr />
+            {
+              paginationInfo &&
+              paginationInfo.pages &&
+              (paginationInfo.pages !== page - 1) &&
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={this.loadNextPage}
+              >
+                Load more
+              </button>
+            }
           </div>
         </section>
       </Layout>
