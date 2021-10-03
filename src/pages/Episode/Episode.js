4202742bd4e98fbb331ce93ce6fe6ac47f977b70
AntonioCopete <antcopjim@gmail.com>
2021-10-03 17:07:33 +0200

    added episode cards, need to improve

--- src/pages/Episode/Episode.js
+++ src/pages/Episode/Episode.js
@@ -11,40 +11,65 @@ class Episode extends Component {
     this.state = {
       episode: null,
       characters: [],
+      hasLoaded: false,
     };
     // ,
     //
-    // hasLoaded: false,
+    //
     // hasError: false,
     // errorMessage: null,
   }
 
   componentDidMount() {
-    axios.get("https://rickandmortyapi.com/api/episode/1").then((data) => {
-      data.data.characters.forEach((element) => {
-        let newElement = element.split("/");
-        newElement = newElement[newElement.length - 1];
-        console.log(newElement);
-      });
+    this.getCharacters();
+  }
 
-      this.setState({
-        episode: data.data.episode,
-        characters: data.data.characters,
+  getOneCharacter(character) {
+    this.setState({ characters: character });
+    return axios.get(character);
+  }
+
+  getCharacters() {
+    const { match } = this.props;
+    const { episodeId } = match.params;
+    axios
+      .get(`https://rickandmortyapi.com/api/episode/${episodeId}`)
+      .then((apiEpisode) => {
+        this.setState({
+          episode: apiEpisode.data.episode,
+        });
+
+        const characterUris = apiEpisode.data.characters;
+
+        axios
+          .all(
+            characterUris.map((char) => {
+              return this.getOneCharacter(char);
+            }),
+          )
+          .then((data) => {
+            const charactersArr = data.map((characterData) => {
+              return characterData.data;
+            });
+            this.setState({ characters: charactersArr, hasLoaded: true });
+          });
       });
-    });
   }
 
   render() {
-    const { episode, characters } = this.state;
-
+    const { episode, characters, hasLoaded } = this.state;
+    setTimeout(console.log(characters), 500);
+    // console.log(characters);
+    console.log(hasLoaded);
+    if (!hasLoaded) {
+      return <div />;
+    }
     return (
       <>
         <Layout>
           <section className="row">
             <div className="col col-12">
-              {/* {characters.map((character, idx) => (
-                
-
+              {characters.map((character) => (
                 <CharacterCard
                   key={character.id}
                   id={character.id}
@@ -55,7 +80,7 @@ class Episode extends Component {
                   origin={character.origin}
                   location={character.location}
                 />
-              ))} */}
+              ))}
             </div>
           </section>
         </Layout>
