7fd20d88238fa8d4c636c91cc32bbfafb14d8e31
sanadriu <skualed@gmail.com>
2021-10-04 10:58:27 +0200

    Updated CharacterProfile

--- src/components/CharacterProfile/CharacterProfile.js
+++ src/components/CharacterProfile/CharacterProfile.js
@@ -15,29 +15,31 @@ export default function CharacterProfile(props) {
 			<div className="col-12 col-sm-6 col-md-4 col-xl-3">
 				<img className="CharacterProfile__img" src={image} alt="" />
 			</div>
-			<div className="col-12 col-sm-6 col-md-8 col-xl-9 row align-content-center">
-				<div className="col-12">
-					<h3>{name}</h3>
-				</div>
-				<div className="col-12">
-					<hr />
-				</div>
-				<div className="col-12 row">
-					<div className="col">
-						<h6>CHARACTER</h6>
-						<p>
-							{species} | {status}
-						</p>
+			<div className="col-12 col-sm-6 col-md-8 col-xl-9 d-flex flex-column justify-content-center">
+				<div className="row">
+					<div className="col-12">
+						<h3>{name}</h3>
 					</div>
-				</div>
-				<div className="col-12 row">
-					<div className="col col-md-4">
-						<h6>ORIGIN</h6>
-						<p>{origin}</p>
+					<div className="col-12">
+						<hr />
 					</div>
-					<div className="col col-md-8">
-						<h6>LOCATION</h6>
-						<p>{location}</p>
+					<div className="col-12">
+						<div className="row">
+							<div className="col-12">
+								<h6>CHARACTER</h6>
+								<p>
+									{species} | {status}
+								</p>
+							</div>
+							<div className="col-12 col-md-4">
+								<h6>ORIGIN</h6>
+								<p>{origin}</p>
+							</div>
+							<div className="col-12 col-md-8">
+								<h6>LOCATION</h6>
+								<p>{location}</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
