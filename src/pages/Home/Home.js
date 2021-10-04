37644221a919de562cf555aad3fcb3a6c3920a04
Paul Molano <paulmolanoo@gmail.com>
2021-10-04 10:51:23 +0200

    Episodies printed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,52 +1,65 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import Layout from "../../components/Layout";
-// import EpisodeCard from "../../components/EpisodeCard";
+import EpisodeCard from "../../components/EpisodeCard";
 
 class Home extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
-    // page: 1,
-    // paginationInfo: null,
-    // episodes: [],
-    // hasLoaded: false,
-    // hasError: false,
-    // errorMessage: null,
+    this.state = {
+      // page: 1,
+      // paginationInfo: null,
+      episodes: [],
+      hasLoaded: true,
+      hasError: false,
+      // errorMessage: null,
+    };
   }
 
   async componentDidMount() {
-    // this.loadEpisodes();
+    await this.loadEpisodes();
   }
 
   async loadEpisodes() {
-    console.log(this);
+    const res = await axios.get("https://rickandmortyapi.com/api/episode");
+    this.setState({
+      episodes: res.data.results,
+    });
   }
 
   render() {
+    const { hasLoaded, hasError, episodes } = this.state;
     return (
       <Layout>
         <section className="row">
-          {/* {hasLoaded && !hasError && (
+          {hasLoaded && !hasError && (
             <div className="col col-12">
               <h1>Episodes loaded!</h1>
             </div>
-          )} */}
+          )}
           <div className="col col-12">
             <hr />
           </div>
-          {/* {episodes.map((episode) => (
-              <EpisodeCard
-                key={episode.id}
-                id={episode.id}
-                name={episode.name}
-                airDate={episode.air_date}
-                episode={episode.episode}
-              />
-            ))} */}
+          {episodes.map((episode) => (
+            <EpisodeCard
+              key={episode.id}
+              id={episode.id}
+              name={episode.name}
+              airDate={episode.air_date}
+              episode={episode.episode}
+            />
+          ))}
           <div className="col col-12">
             <hr />
+          </div>
+          <div>
+            <button type="button" className="btn btn-primary m-1">
+              Previous page
+            </button>
+            <button type="button" className="btn btn-primary m-1">
+              Next page
+            </button>
           </div>
         </section>
       </Layout>
