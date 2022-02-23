ee31916572589d54e85dc8204ffd8cd0bec4bf21
Luis Molina <luismateo2416@gmail.com>
2022-02-23 13:18:47 +0100

    first fetch to spawn the episodes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,42 +1,63 @@
+import axios from "axios";
 import React, { Component } from "react";
 
 import Layout from "../../components/Layout";
-// import EpisodeCard from "../../components/EpisodeCard";
+import EpisodeCard from "../../components/EpisodeCard";
 
 class Home extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
-    // page: 1,
-    // paginationInfo: null,
-    // episodes: [],
-    // hasLoaded: false,
-    // hasError: false,
-    // errorMessage: null,
+    this.state = {
+      page: 1,
+      paginationInfo: null,
+      episodes: [],
+      hasLoaded: true,
+      hasError: false,
+      errorMessage: null,
+    };
   }
 
   async componentDidMount() {
-    // this.loadEpisodes();
+    const { page } = this.state
+    await this.loadEpisodes(page);
+    this.setState(prevState => ({
+      ...prevState,
+      isLoading: false
+    }))
+
   }
 
-  async loadEpisodes() {
+  async loadEpisodes(page) {
     console.log(this);
+    await axios.get(`https://rickandmortyapi.com/api/episode?page=${page}`)
+      .then(data => {
+        const dataRequest = data.data
+        return (
+          this.setState((prevState) => ({
+            ...prevState,
+            paginationInfo: dataRequest.info,
+            episodes: dataRequest.results,
+          }))
+        );
+      })
   }
 
   render() {
+    const { paginationInfo, episodes, hasError, hasLoaded } = this.state
     return (
       <Layout>
         <section className="row">
-          {/* {hasLoaded && !hasError && (
+          {hasLoaded && !hasError && (
             <div className="col col-12">
               <h1>Episodes loaded!</h1>
             </div>
-          )} */}
+          )}
           <div className="col col-12">
             <hr />
           </div>
-          {/* {episodes.map((episode) => (
+          {
+            episodes.map((episode) => (
               <EpisodeCard
                 key={episode.id}
                 id={episode.id}
@@ -44,7 +65,8 @@ class Home extends Component {
                 airDate={episode.air_date}
                 episode={episode.episode}
               />
-            ))} */}
+            ))
+          }
           <div className="col col-12">
             <hr />
           </div>
